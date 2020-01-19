4654d77497d974a382ca480c8be1fa33e36f9e30
Sumeet Kumar <sumeet@grappus.com>
2020-01-19 19:17:47 +0530

    Added a dummy button to add more cards.

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,28 @@ class MyCoolApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food_paradise.jpeg'),
-              Text('Food Paradise')
-            ],
+          appBar: AppBar(
+            title: Text('EasyList'),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Card'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food_paradise.jpeg'),
+                    Text('Food Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
